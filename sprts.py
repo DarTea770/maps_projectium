6320b8a4d7bd7837bea25b448c1ad85c9e332678
DarTea770 <mark2love2@yandex.ru>
2022-02-01 18:04:31 +0300

    now you can move the map with W,A,S,D keys! Also some fixes in map scaling.

--- sprts.py
+++ sprts.py
@@ -1,9 +1,9 @@
 import os
-
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtCore import Qt
 import sys
 import requests
+
 
 def get_img(map_request):
     response = requests.get(map_request)
@@ -12,7 +12,6 @@ def get_img(map_request):
         print(map_request)
         print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
-    # Запишем полученное изображение в файл.
     map_file = "map.png"
     os.remove(map_file)
     map_file = "map.png"
@@ -29,7 +28,8 @@ class Widget(QtWidgets.QMainWindow):
         self.setGeometry(0, 0, self.w - 30, self.h - 30)
 
         self.map_size = 0.0016
-        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll=37.677564,55.653309&spn={self.map_size},{self.map_size}&size=650,450"
+        self.coords = [37.618764, 55.759626]
+        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll={self.coords[0]},{self.coords[1]}&spn={self.map_size},{self.map_size}&size=650,450"
         self.pixmap = QtGui.QPixmap(get_img(request)).scaled(self.w - 30, self.h, Qt.IgnoreAspectRatio)
         self.image = QtWidgets.QLabel(self)
         self.image.move(0, 0)
@@ -53,27 +53,97 @@ class Widget(QtWidgets.QMainWindow):
             self.size_map_up()
         if event.key() == Qt.Key_Minus:
             self.size_map_down()
+        if event.key() == Qt.Key_A:
+            self.move_map_right()
+        if event.key() == Qt.Key_D:
+            self.move_map_left()
+        if event.key() == Qt.Key_W:
+            self.move_map_up()
+        if event.key() == Qt.Key_S:
+            self.move_map_down()
 
     def size_map_up(self):
         if self.map_size >= 0.0007:
-            if self.map_size >= 0.01:
-                self.map_size = round(self.map_size - 0.1, 4)
+            if self.map_size >= 0.5:
+                self.map_size = round(self.map_size - 0.4, 4)
+            elif self.map_size >= 0.1:
+                self.map_size = round(self.map_size - 0.08, 4)
+            elif self.map_size >= 0.01:
+                self.map_size = round(self.map_size - 0.006 if self.map_size < 0.02 else self.map_size - 0.02, 4)
             elif self.map_size >= 0.0045:
-                self.map_size = round(self.map_size - 0.003, 4)
+                self.map_size = round(self.map_size - 0.004, 4)
             else:
                 self.map_size = round(self.map_size - 0.001, 4)
-        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll=37.677564,55.653309&spn={self.map_size},{self.map_size}&size=650,450"
+        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll={self.coords[0]},{self.coords[1]}&spn={self.map_size},{self.map_size}&size=650,450"
         pixmap = QtGui.QPixmap(get_img(request)).scaled(self.w - 30, self.h, Qt.IgnoreAspectRatio, Qt.FastTransformation)
         self.image.setPixmap(pixmap)
 
     def size_map_down(self):
-        if self.map_size <= 0.5:
-            self.map_size += 0.002 if self.map_size < 0.006 else 0.1
+        if self.map_size <= 1.5:
+            if self.map_size < 0.006:
+                self.map_size += 0.002
+            elif self.map_size < 0.02:
+                self.map_size += 0.009
+            elif self.map_size < 0.5:
+                self.map_size += 0.1
+            else:
+                self.map_size += 0.4
             self.map_size = round(self.map_size, 4)
-        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll=37.677564,55.653309&spn={self.map_size},{self.map_size}&size=650,450"
+        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll={self.coords[0]},{self.coords[1]}&spn={self.map_size},{self.map_size}&size=650,450"
         pixmap = QtGui.QPixmap(get_img(request)).scaled(self.w - 30, self.h, Qt.IgnoreAspectRatio, Qt.FastTransformation)
         self.image.setPixmap(pixmap)
 
+    def move_map_left(self):
+        if self.map_size > 0.3:
+            self.coords[0] = self.coords[0] + 0.1
+        elif self.map_size > 0.1:
+            self.coords[0] = self.coords[0] + 0.05
+        else:
+            self.coords[0] = self.coords[0] + 0.004
+        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll={self.coords[0]},{self.coords[1]}&spn={self.map_size},{self.map_size}&size=650,450"
+        pixmap = QtGui.QPixmap(get_img(request)).scaled(self.w - 30, self.h, Qt.IgnoreAspectRatio,
+                                                        Qt.FastTransformation)
+        self.image.setPixmap(pixmap)
+
+    def move_map_right(self):
+        if self.map_size > 0.3:
+            self.coords[0] = self.coords[0] - 0.1
+        elif self.map_size > 0.1:
+            self.coords[0] = self.coords[0] - 0.05
+        else:
+            self.coords[0] = self.coords[0] - 0.004
+        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll={self.coords[0]},{self.coords[1]}&spn={self.map_size},{self.map_size}&size=650,450"
+        pixmap = QtGui.QPixmap(get_img(request)).scaled(self.w - 30, self.h, Qt.IgnoreAspectRatio,
+                                                        Qt.FastTransformation)
+        self.image.setPixmap(pixmap)
+
+    def move_map_up(self):
+        if self.map_size > 0.8:
+            self.coords[1] = self.coords[1] + 0.3
+        elif self.map_size > 0.3:
+            self.coords[1] = self.coords[1] + 0.1
+        elif self.map_size > 0.1:
+            self.coords[1] = self.coords[1] + 0.05
+        else:
+            self.coords[1] = self.coords[1] + 0.003
+        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll={self.coords[0]},{self.coords[1]}&spn={self.map_size},{self.map_size}&size=650,450"
+        pixmap = QtGui.QPixmap(get_img(request)).scaled(self.w - 30, self.h, Qt.IgnoreAspectRatio,
+                                                        Qt.FastTransformation)
+        self.image.setPixmap(pixmap)
+
+    def move_map_down(self):
+        if self.map_size > 0.8:
+            self.coords[1] = self.coords[1] - 0.3
+        elif self.map_size > 0.3:
+            self.coords[1] = self.coords[1] - 0.1
+        elif self.map_size > 0.1:
+            self.coords[1] = self.coords[1] - 0.05
+        else:
+            self.coords[1] = self.coords[1] - 0.003
+        request = f"https://static-maps.yandex.ru/1.x/?l=sat&ll={self.coords[0]},{self.coords[1]}&spn={self.map_size},{self.map_size}&size=650,450"
+        pixmap = QtGui.QPixmap(get_img(request)).scaled(self.w - 30, self.h, Qt.IgnoreAspectRatio,
+                                                        Qt.FastTransformation)
+        self.image.setPixmap(pixmap)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
